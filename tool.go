5e064ce31919fca9ce5eedae28397fbc56b382ef
Tyler <tvanden@live.com>
2023-02-16 00:00:30 -0700

    add ability to load a file into the tool, so you can essentially edit it. It will still only save to "gameoflife.txt" so if you want to save one, you have to rename it.

--- tool.go
+++ tool.go
@@ -3,6 +3,9 @@ package main
 import (
   "os"
   "fmt"
+  "flag"
+  "strings"
+  "io/ioutil"
   "github.com/gdamore/tcell/v2"
 )
 
@@ -59,6 +62,50 @@ func save(data [][]int) {
   }
 }
 
+// Reads a file as a start
+func readFile(file string) [][]int {
+  f, _ := ioutil.ReadFile(file)
+
+  lines := strings.Split(string(f), "\n")
+
+  var cleanLines []string
+  var data [][]int
+
+  // Remove the leading [ and the trailing ] of each line and remove
+  // empty lines
+  for _, line := range lines {
+    newline1 := strings.Replace(line, "[", "", -1)
+    newline2 := strings.Replace(newline1, "]", "", -1)
+    newline3 := strings.Replace(newline2, " ", "", -1)
+    if len(line) > 0 {
+      cleanLines = append(cleanLines, newline3)
+    }
+  }
+
+  // Create an array of arrays with the proper size for the data. Fill with 0's
+  for i := 0; i < len(cleanLines); i++ {
+    var newSlice []int
+    data = append(data, newSlice)
+    for j := 0; j < len(cleanLines[0]); j++ {
+      newInt := 0
+      data[i] = append(data[i], newInt)
+    }
+  }
+
+  // Go over the string data, and convert it to the int data. Only have to update
+  // the 1's
+  for i, line := range cleanLines {
+    for j, char := range line {
+      if char == '1' {
+        data[i][j] = 1
+      }
+    }
+  }
+  return data
+
+}
+
+
 func writeToScreen(s tcell.Screen, style tcell.Style, x int, y int, str string) {
   for i, char := range str {
     s.SetContent(x+i, y, rune(char), nil, style)
@@ -66,6 +113,14 @@ func writeToScreen(s tcell.Screen, style tcell.Style, x int, y int, str string) 
 }
 
 func main() {
+
+  var file string
+
+  flag.StringVar(&file, "f", "", "File to read from")
+
+  flag.Parse()
+
+
   s, err := tcell.NewScreen()
   if err != nil {
     fmt.Println("Error in tcell.NewScreen:", err)
@@ -77,7 +132,13 @@ func main() {
   }
 
 
-  data := createEmptySlice(s)
+  var data [][]int
+
+  if file != "" {
+    data = readFile(file)
+  } else {
+    data = createEmptySlice(s)
+  }
 
   style := tcell.StyleDefault.Foreground(tcell.ColorWhite)
   style2 := tcell.StyleDefault.Foreground(tcell.ColorGreen)
@@ -86,9 +147,10 @@ func main() {
   s.EnableMouse()
 
   s.Clear()
+  drawSlice(s, style2, data)
+  s.Sync()
 
   for {
-    s.Clear()
     switch ev := s.PollEvent().(type) {
     case *tcell.EventResize:
       s.Sync()
@@ -112,13 +174,13 @@ func main() {
       }
     case *tcell.EventMouse:
       xPos, yPos := ev.Position()
+      s.Clear()
       s.SetContent(xPos, yPos, tcell.RuneBlock, nil, style)
-
       drawSlice(s, style2, data)
       s.Show()
       switch ev.Buttons() {
       case tcell.Button1:
-        updateData(xPos,yPos, data)
+        updateData(xPos, yPos, data)
         s.SetContent(xPos, yPos, tcell.RuneBlock, nil, style2)
         s.Show()
       }
